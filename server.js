b52d142171d09ca455d5c4b572bf9133694500af
veera venky <veeravenky222@gmail.com>
2018-09-13 14:53:02 +0530

    Added heroku support for PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var handler = require('./lib/controller');
 
 var games=new Games();
 
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || 4000;
+var PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 4000;
 var IP = process.env.OPENSHIFT_NODEJS_IP;
 
 var server = http.createServer(function(req,res){
